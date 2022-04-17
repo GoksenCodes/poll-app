d3cfe054a679690bed1b588f97538087fb1b161f
Goksen <goksengorgulu@gmail.com>
2022-04-17 14:05:48 +0200

    create option service to store options data

--- src/app/services/options.service.ts
+++ src/app/services/options.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OptionsService {
-  respondentOptions: string[];
+  respondentOptions: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(
+    []
+  );
 
   constructor() {}
 }
