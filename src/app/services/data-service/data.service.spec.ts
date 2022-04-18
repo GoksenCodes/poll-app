097c1eb9cdcfe8d9d6937eda627d7700db683c6d
Goksen <goksengorgulu@gmail.com>
2022-04-18 02:15:38 +0200

    simplfy data storing by merging services in one and updating the methods to get one way data flow

--- src/app/services/data-service/data.service.spec.ts
+++ src/app/services/data-service/data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { OptionsService } from './options.service';
+import { DataService } from './data.service';
 
-describe('OptionsService', () => {
-  let service: OptionsService;
+describe('DataService', () => {
+  let service: DataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(OptionsService);
+    service = TestBed.inject(DataService);
   });
 
   it('should be created', () => {
---
+++ src/app/services/data-service/data.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { IOptionObject } from 'src/app/models/option-object.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private votesSource: BehaviorSubject<IOptionObject[]> = new BehaviorSubject<
+    IOptionObject[]
+  >([]);
+  votes = this.votesSource.asObservable();
+
+  private optionsSource: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(
+    []
+  );
+  options = this.optionsSource.asObservable();
+
+  constructor() { }
+
+  updateVotingData(votes: IOptionObject []) {
+    this.votesSource.next(votes)
+  }
+  
+  updateOptionsData(options: string[]) {
+    this.optionsSource.next(options)
+  }
+
+  
+}
