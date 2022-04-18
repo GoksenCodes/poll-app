5cf8ae0418c912f8dfd0dcc5ab751c591639411a
Goksen <goksengorgulu@gmail.com>
2022-04-18 02:16:42 +0200

    fix votingCount data

---
+++ src/app/components/voting/voting.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { IOptionObject } from 'src/app/models/option-object.model';
+import { DataService } from 'src/app/services/data-service/data.service';
+
+@Component({
+  selector: 'app-voting',
+  templateUrl: './voting.component.html',
+  styleUrls: ['./voting.component.scss']
+})
+export class VotingComponent implements OnInit {
+  votingOptions: string[];
+  optionForm: FormGroup;
+  voteCounts: IOptionObject[] = [];
+  arrayOfObjects = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private dataService: DataService
+  ) {
+    this.optionForm = this.createOptionForm();
+  }
+
+  ngOnInit(): void {
+    this.dataService.options.subscribe(options => {
+      this.votingOptions = options;
+
+      this.voteCounts = options.map(o => ({ value: o, count: 0 }))
+    })
+  }
+
+  createOptionForm(): FormGroup {
+    return this.fb.group({
+      option: [null]
+    });
+  }
+
+  submit() {
+    const votedOption = this.optionForm.value.option;
+    this.updateVotes(votedOption);
+    console.log(this.votingOptions)
+    this.optionForm.reset();
+  }
+
+  updateVotes(votedOption: string) {
+    this.voteCounts.map(v => {
+      if (v.value === votedOption) {
+        v.count++
+      }
+    })
+    this.dataService.updateVotingData(this.voteCounts)
+  }
+}
